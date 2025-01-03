fd3dca6944a0f72c6bec6e4fce3e0425c782791f
williamldk <williamclm_christian@hotmail.com>
2025-01-03 02:07:19 -0300

    Integração com o while

--- aula 3/logica-js-aula_2/app.js
+++ aula 3/logica-js-aula_2/app.js
@@ -1,15 +1,19 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
+//enquanto o chute no for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
     } else {
-        alert(`O número secreto é maior que ${chute}`)
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
     }
 }
