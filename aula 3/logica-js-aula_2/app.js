c9b9122f323e40fead76d0d6181befbc796a9dc5
williamldk <williamclm_christian@hotmail.com>
2025-01-03 01:40:30 -0300

    Novas condições

--- aula 3/logica-js-aula_2/app.js
+++ aula 3/logica-js-aula_2/app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
+let numeroSecreto = 5;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
@@ -7,5 +7,9 @@ let chute = prompt('Escolha um número entre 1 e 10');
 if (chute == numeroSecreto) {
     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    alert('Você errou :(')
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`)
+    }
 }
