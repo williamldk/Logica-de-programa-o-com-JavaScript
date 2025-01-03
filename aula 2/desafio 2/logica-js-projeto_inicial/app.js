735f432a095689d22f1d84586dcab119f9f93f30
williamldk <williamclm_christian@hotmail.com>
2025-01-03 01:14:51 -0300

    verificador do numero (positivo/negativo)

--- aula 2/desafio 2/logica-js-projeto_inicial/app.js
+++ aula 2/desafio 2/logica-js-projeto_inicial/app.js
@@ -6,3 +6,9 @@ if (diaDaSemana == 'Sábado') {
 } else {
     alert('Boa semana!');
 }
+numero = prompt('Digite um positivo ou negativo');
+if (numero > 0) {
+    alert('Número positivo!');
+} else {
+    alert('Número negativo!');
+}
