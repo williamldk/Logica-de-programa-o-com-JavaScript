11ae42ea1c302cbdb376bbf63580c99e3c502e27
williamldk <williamclm_christian@hotmail.com>
2025-01-05 22:30:57 -0300

    Sistema de pontuação

--- aula 3/desafio 3 - Hora da pratica/logica-js-projeto_inicial/app.js
+++ aula 3/desafio 3 - Hora da pratica/logica-js-projeto_inicial/app.js
@@ -11,3 +11,12 @@ if (numero > 0) {
 } else {
     alert('Número negativo!');
 }
+//Crie um sistema de pontuação para um jogo. 
+// Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!" no console do navegador. 
+// Caso contrário, mostre "Tente novamente para ganhar.".
+pontuacao = 105;
+if (pontuacao >= 100) {
+    console.log('Parabéns, você venceu!');
+} else {
+    console.log('Tente novamente para ganhar.');
+}
