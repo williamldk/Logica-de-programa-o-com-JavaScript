be2a59a1b65832f7a7f3dd096323cc90307ae82b
williamldk <williamclm_christian@hotmail.com>
2025-01-05 22:29:06 -0300

    verificação de positivo ou negativo

--- aula 3/desafio 3 - Hora da pratica/logica-js-projeto_inicial/app.js
+++ aula 3/desafio 3 - Hora da pratica/logica-js-projeto_inicial/app.js
@@ -4,4 +4,10 @@ while (contador >= 0) {
     console.log(contador);
     contador++
 }
-
+//Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+numero = prompt('Digite um positivo ou negativo');
+if (numero > 0) {
+    alert('Número positivo!');
+} else {
+    alert('Número negativo!');
+}
